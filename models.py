9c8d626ff4a3c35dde07c1a0c9af1be45b0cf60c
mattdmayberry <mattdmayberry@gmail.com>
2016-05-22 15:47:47 -0700

    Added header to layout template

--- models.py
+++ models.py
@@ -27,7 +27,7 @@ class User(UserMixin, Model):
     username = CharField(unique=True)
     email = CharField(unique=True)
     password = CharField(max_length=100)
-    joined_at = DateTimeField(default=datetime.datetime.now)
+    joined_at = DateTimeField(default=datetime.datetime.now())
     is_admin = BooleanField(default=False)
 
     class Meta:
