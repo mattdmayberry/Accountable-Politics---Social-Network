2adc8972f09fde54c3af42f2f6c8f2436206eab3
matt <mattdmayberry@gmail.com>
2016-05-27 01:17:50 +0000

    Created test_app.py with unit testing for app.py

---
+++ test_app.py
@@ -0,0 +1,65 @@
+#Accountable Politics
+#test_app.py
+
+import unittest
+import tempfile
+import requests
+from app import *
+
+
+class Test_App(unittest.TestCase):
+    
+    render_templates = False
+    
+    @classmethod
+    def setUpClass(self):
+        print("Initializing Test Instances...")
+
+        # create a tempfile db
+        self.db_fd, app.config['DATABASE'] = tempfile.mkstemp()
+        self.app = app.test_client()
+        
+        # self.form = forms.RegisterForm()    
+        # self.form.username.data = "test_user"
+        # self.form.email.data = "test_user@example.com"
+        # self.form.password.data = "test_password"
+        # self.form.password2.data = "test_password"
+
+        self.formPayload = {'username':'test_user', 
+                            'email':"test_user@example.com",
+                            "password":"test_password",
+                            "password2":"test_password"}
+    @classmethod
+    def tearDownClass(self):
+        print("Deleting Test Instances")
+        os.close(self.db_fd)
+        os.unlink(app.config['DATABASE'])
+    
+    def register(self):
+        return requests.post('/register', params=self.formPayload)
+        
+    # def register_form(self, Form, username, email, password):
+    #     self.form.username = username
+    #     self.form.email = email
+    #     self.form.password, self.form.password2 = password
+        
+    # def login(self, username, password):
+    #     return self.app.post('/login', data = dict(
+    #         username = username,
+    #         password = password),
+    #         follow_redirect = True)
+    
+    # def logout(self):
+    #     return self.app.get('/logout', follow_redirect = True)
+    
+    def test_register(self):
+        rv = self.register()
+        print(rv.data.decode("UTF-8"))
+        assert "You successfully signed up!" in rv.data.decode("UTF-8")
+        
+    # def test_login_logout(self):
+    #     rv = self.login("test_models_user", "test_models_user_pw")
+    #     assert "You are logged in!" in rv.data
+        
+if __name__ == '__main__':
+   unittest.main()
